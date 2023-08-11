c1878744a792cafefe619a890834f55f8843c630
Alex Christian <alexchristianqr@gmail.com>
2023-08-11 17:34:09 -0500

    [updated] dto

--- src/examples/dto/update-example.dto.ts
+++ src/examples/dto/update-example.dto.ts
@@ -1,5 +1,4 @@
 import { PartialType } from "@nestjs/mapped-types";
 import { ExampleEntity } from "../entities/example.entity";
-import { IsNotEmpty } from "class-validator";
 
 export class UpdateExampleDto extends PartialType(ExampleEntity) {}
